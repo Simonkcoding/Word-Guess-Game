64875740ba18b3ea6ada0833c6ccb704b8e5c7d4
simonkcoding <ykwok3@uncc.edu>
2018-09-10 09:10:31 -0400

    inital site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,14 +14,14 @@ var wordList = [
 //declare quiz alphabet array and masked array
 var wordArray = [];
 var maskedArray = [];
-var winCount;
+var winCount = 0; // Set win count = 0;
 var guessCount;
 var wordUsed = [];
 var quizChoice;
 var bgSound;
 
 
-//
+//====== functions =======//
 // Program Start function
 function gameStart() {
     //Create random choice
@@ -39,95 +39,120 @@ function gameStart() {
     guessCount = 9;
     //reset guess
     wordUsed = [];
-    
 };
-
 //Run once when page loaded
 gameStart();
-// Set win count = 0;
-winCount = 0;
+
+//
 //Sound function
 function sound(src) {
     bgSound = document.createElement("iframe");
     bgSound.src = src;
-    bgSound.setAttribute("allow","autoplay");
+    bgSound.setAttribute("allow", "autoplay");
     bgSound.style.display = "none";
     document.body.appendChild(bgSound);
 }
 
+//
+//Alphabet function
+var alphabetArray = [];
+function alpha(charA, charZ) {
+
+    var j = charA.charCodeAt(0);
+    var k = charZ.charCodeAt(0);
+    for (; j <= k; j++) {
+        alphabetArray.push(String.fromCharCode(j));
+    }
+    return alphabetArray;
+};
+//Run once when page loaded
+alpha('A', 'Z');
+console.log(alphabetArray);
+
+
+//
 // Game Start
 document.onkeyup = function (event) {
-//Turn key to upper case
-var eventKeyUpper = event.key.toUpperCase();
+    
+
+    //Turn key to upper case
+    var eventKeyUpper = event.key.toUpperCase();
+    var charCodeCheck = eventKeyUpper.charCode;
+    // console.log(event.which);
+
+if  (event.which  >= 65 && event.which  <= 90) {
     wordUsed.push(eventKeyUpper);
-    console.log(wordUsed);
+    // console.log(wordUsed);
     var elementWrongCounter = 0;
-    var i;
-    for (i = 0; i < wordArray.length; i++) {
-        if (eventKeyUpper == wordArray[i]) {
-            maskedArray[i] = eventKeyUpper;
-            console.log(maskedArray);
-            console.log(wordArray);
-            if (maskedArray.toString() == wordArray.toString()) {
-                alert('Good job, the answer is '+ quizChoice);
-                winCount++;
-                if(quizChoice=="MERCURY"){
-                    document.getElementById("planet").src = "assets/images/mercury.jpg";
-                    bgSound = sound("assets/media/mercury.mp3");
+    
+  
+            
+            
+            for (var i = 0; i < wordArray.length; i++) {
+                if (eventKeyUpper == wordArray[i]) {
+                    maskedArray[i] = eventKeyUpper;
+                    console.log(maskedArray);
+                    console.log(wordArray);
+                    if (maskedArray.toString() == wordArray.toString()) {
+                        alert('Good job, the answer is ' + quizChoice);
+                        winCount++;
+                        if (quizChoice == "MERCURY") {
+                            document.getElementById("planet").src = "assets/images/mercury.jpg";
+                            bgSound = sound("assets/media/mercury.mp3");
+                        }
+                        if (quizChoice == "VENUS") {
+                            document.getElementById("planet").src = "assets/images/venus.jpg";
+                            bgSound = sound("assets/media/venus.mp3");
+                        }
+                        if (quizChoice == "EARTH") {
+                            document.getElementById("planet").src = "assets/images/earth.jpg";
+                            bgSound = sound("assets/media/earth.mp3");
+                        }
+                        if (quizChoice == "MARS") {
+                            document.getElementById("planet").src = "assets/images/mars.jpg";
+                            bgSound = sound("assets/media/mars.mp3");
+                        }
+                        if (quizChoice == "JUPITER") {
+                            document.getElementById("planet").src = "assets/images/jupitor.jpg";
+                            bgSound = sound("assets/media/jupitor.mp3");
+                        }
+                        if (quizChoice == "SATURN") {
+                            document.getElementById("planet").src = "assets/images/saturn.jpg";
+                            bgSound = sound("assets/media/saturn.mp3");
+                        }
+                        if (quizChoice == "URANUS") {
+                            document.getElementById("planet").src = "assets/images/uranus.jpg";
+                            bgSound = sound("assets/media/uranus.mp3");
+                        }
+                        if (quizChoice == "NEPTUNE") {
+                            document.getElementById("planet").src = "assets/images/neptune.jpg";
+                            bgSound = sound("assets/media/neptune.mp3");
+                        }
+                        // console.log(winCount);
+                        gameStart();
+                    }
+                } else {
+                    elementWrongCounter++;
                 }
-                if(quizChoice=="VENUS"){
-                    document.getElementById("planet").src = "assets/images/venus.jpg";
-                    bgSound = sound("assets/media/venus.mp3");
+                if (elementWrongCounter == wordArray.length) {
+                    --guessCount;
+                    // console.log(guessCount);
                 }
-                if(quizChoice== "EARTH"){
-                    document.getElementById("planet").src = "assets/images/earth.jpg";
-                    bgSound = sound("assets/media/earth.mp3");
+                if (guessCount == 0) {
+                    alert('Game Over, the answer is "' + quizChoice + '". Game restart!');
+                    gameStart();
                 }
-                if(quizChoice== "MARS"){
-                    document.getElementById("planet").src = "assets/images/mars.jpg";
-                    bgSound = sound("assets/media/mars.mp3");
-                }
-                if(quizChoice=="JUPITER"){
-                    document.getElementById("planet").src = "assets/images/jupitor.jpg";
-                    bgSound = sound("assets/media/jupitor.mp3");
-                }
-                if(quizChoice== "SATURN"){
-                    document.getElementById("planet").src = "assets/images/saturn.jpg";
-                    bgSound = sound("assets/media/saturn.mp3");
-                }
-                if(quizChoice== "URANUS"){
-                    document.getElementById("planet").src = "assets/images/uranus.jpg";
-                    bgSound = sound("assets/media/uranus.mp3");
-                }
-                if(quizChoice=="NEPTUNE"){
-                    document.getElementById("planet").src = "assets/images/neptune.jpg";
-                    bgSound = sound("assets/media/neptune.mp3");
-                }
-                console.log(winCount);
-                gameStart();    
+
             }
-        } else {
-            elementWrongCounter++;
+
+            var getMaskedArray = document.getElementById('answerArray-Text');
+            var getGuessLeft = document.getElementById('guessLeftCount-Text');
+            var getwinCount = document.getElementById('winCount-Text');
+            var getwordUsed = document.getElementById('guessSoFar-Text');
+
+            getMaskedArray.textContent = maskedArray;
+            getGuessLeft.textContent = guessCount;
+            getwinCount.textContent = winCount;
+            getwordUsed.textContent = wordUsed;
         }
-        if (elementWrongCounter == wordArray.length) {
-            --guessCount;
-            console.log(guessCount);
-        }
-        if (guessCount == 0) {
-            alert('Game Over, the answer is "'+ quizChoice +'". Game restart!');
-            gameStart();
-        }
-
-    }
-
-
-    var getMaskedArray = document.getElementById('answerArray-Text');
-    var getGuessLeft = document.getElementById('guessLeftCount-Text');
-    var getwinCount = document.getElementById('winCount-Text');
-    var getwordUsed = document.getElementById('guessSoFar-Text');
-
-    getMaskedArray.textContent = maskedArray;
-    getGuessLeft.textContent = guessCount;
-    getwinCount.textContent = winCount;
-    getwordUsed.textContent = wordUsed;
 };
